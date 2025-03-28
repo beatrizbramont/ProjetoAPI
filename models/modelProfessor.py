0445ad371281dbbf50c6c690c4118c56828a2086
Isadora <isadora.silva@aluno.faculdadeimpacta.com.br>
2025-03-28 14:18:19 -0300

    fix: modelProfessor ajustado

--- models/modelProfessor.py
+++ models/modelProfessor.py
@@ -12,23 +12,42 @@ dici = {
     ]
 }
 
+#CREATE
+
 def createProfessores(dados):
-    try:
-       
-        vazio = verificar_campo_null(dados)
-        if vazio:
-            return vazio, 400
-        
-        duplicacao = verificar_duplicacao(dados['id'], dici["professor"], "Professor")
-        if duplicacao:
-            return duplicacao
-        
+   
         dici['professor'].append(dados)
-        return (dados), 201
-    
-    except Exception as e:
-        return ({"error": str(e)}), 500
-    
+        return True
+
+#GET
+
+def todosProfessores():
+      return dici['professor']
 
 def professor_porID(id_professor):
-    return id_professor
+    lista_professores = dici['professor']
+    for professor in lista_professores:
+          if professor['id'] == id_professor:
+                return professor
+    return False
+
+
+#PUT 
+
+def updateProfessor(idProfessor, dados):
+    professor = professor_porID(idProfessor)
+    if professor:
+        professor.update(dados)
+        return professor
+    
+    return False
+
+#DELETE
+
+def deleteProfessor(idProfessor):
+    professor = professor_porID(idProfessor)
+    if professor:
+        dici['professor'].remove(professor)
+        return True
+    
+    return False
