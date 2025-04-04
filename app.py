f4d2a49355af457e9a295010b7cebe6d292ad986
Isadora <isadora.silva@aluno.faculdadeimpacta.com.br>
2025-04-04 16:32:41 -0300

    feat: app - atualizado

--- app.py
+++ app.py
@@ -1,75 +1,15 @@
-from flask import Flask, jsonify, request
-from models import modelAluno, modelProfessor, modelTurma
+import os
+from config import app
 
-app = Flask(__name__)
+from controller.routesAlunos import alunos_bp
+from controller.routesProfessor import professor_bp
+from controller.routesTurma import turma_bp
 
-
-# POST (CREATE)
-
-@app.route('/turma', methods=['POST'])
-def createTurma():
-    try:
-        dados = request.json
-
-        modelTurma.createTurma(dados)
-        return jsonify(dados), 201
-        
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
-
-    
-
-    
-# GET (READ) - PROFESSOR
-
-    
-# GET (READ) - TURMA    
-@app.route('/turma', methods=['GET'])
-def getTurma():
-    try: 
-        return modelTurma.todasTurmas() 
-    except Exception as e:
-        return jsonify ({"error": str(e)}), 500
-
-@app.route('/turma/<int: id_turma>', methods=['GET'])
-def turma_Id(id_turma):
-    try:
-        modelTurma.turmaPorID(id_turma)
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
-    
-# PUT (UPDATE)
-
-
-
-
-@app.route("/turma/<int:idTurma>", methods=['PUT'])
-def updateTurma(idTurma):
-    try:
-        dados = request.json
-
-        turma = modelTurma.updateTurma(idTurma, dados)
-        return jsonify(turma)
-
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
-
-
-#DELETE
-
-    
- 
-    
-
-@app.route('/turma/<int:idTurma>', methods=['DELETE'])
-def delete_turma(idTurma):
-    modelTurma.deleteTurma(idTurma)
-    if modelTurma.deleteTurma() == True:
-        return jsonify({"Turma excluída com sucesso."}), 200
-    else:
-        return jsonify({"Turma não encontrada."}), 404
-
-
+# Registrar os blueprints
+app.register_blueprint(alunos_bp)
+app.register_blueprint(professor_bp)
+app.register_blueprint(turma_bp)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host=app.config["HOST"], port = app.config['PORT'],debug=app.config['DEBUG'])
+
