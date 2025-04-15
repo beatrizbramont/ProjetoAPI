c43081f17508b1cd30fcbd266021d48f09a00ac7
Beatriz <beatriz.bramont@gmail.com>
2025-04-15 18:36:12 -0300

    fix: routesProfessor - atualização do código com banco de dados (sqlalchemy)

--- controller/routesProfessor.py
+++ controller/routesProfessor.py
@@ -9,9 +9,8 @@ def createProfessores():
     try:
         dados = request.json
 
-        modelProfessor.createProfessores(dados)          
-        return jsonify(dados), 200
-    
+        professor = modelProfessor.createProfessor(dados)          
+        return professor
     except Exception as e:
         return jsonify({"error": str(e)}), 500
     
@@ -26,7 +25,7 @@ def getProfessor():
 @professor_bp.route('/professor/<int:id_professor>', methods=['GET'])
 def professor_Id(id_professor):
     try:
-        modelProfessor.professor_porID(id_professor)
+        return modelProfessor.professorPorID(id_professor)
     except Exception as e:
         return jsonify({"error": str(e)}), 500
     
@@ -46,8 +45,12 @@ def updateProfessores(idProfessor):
    
 @professor_bp.route('/professor/<int:idProfessor>', methods=['DELETE'])
 def delete_professor(idProfessor):
-    if modelProfessor.deleteProfessor(idProfessor) == True:
-        return jsonify({"message": "Professor excluído com sucesso"}), 200
-    
-    else:
-        return ({"message": "Professor não encontrado"}), 404
+    try:
+        result = modelProfessor.deleteProfessor(idProfessor)
+        if result:  # Se retornou um JSON de sucesso
+            return jsonify({"message": "Professor excluído com sucesso"}), 200
+        else:
+            return jsonify({"message": "Professor não encontrado"}), 404
+        
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
