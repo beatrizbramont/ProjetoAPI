64ce03df36dd819392c475885f5c9fa98ed92a6c
Beatriz <beatriz.bramont@gmail.com>
2025-04-15 18:34:33 -0300

    fix: app - Atualizando código para banco de dados

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
-from config import app
-from testeTDD import runTests
-
+import os
+from config import app, db
 from controller.routesAlunos import alunos_bp
 from controller.routesProfessor import professor_bp
 from controller.routesTurma import turma_bp
@@ -10,6 +9,8 @@ app.register_blueprint(alunos_bp)
 app.register_blueprint(professor_bp)
 app.register_blueprint(turma_bp)
 
+with app.app_context():
+    db.create_all()
 
 if __name__ == '__main__':
     app.run(host=app.config["HOST"], port = app.config['PORT'],debug=app.config['DEBUG'])
