54064aab2f0d5858efe54a76da45e0bee51ee4fd
Beatriz <beatriz.bramont@gmail.com>
2025-04-16 17:37:20 -0300

    fix: app - alteração para funcionamento do bd

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ app.register_blueprint(turma_bp)
 
 with app.app_context():
     db.create_all()
+    
 
 if __name__ == '__main__':
     app.run(host=app.config["HOST"], port = app.config['PORT'],debug=app.config['DEBUG'])
