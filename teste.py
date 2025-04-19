f517ed9fc58040f4e7e2f4b428381090cb2ec988
Beatriz <beatriz.bramont@gmail.com>
2025-04-19 00:32:03 -0300

    fix: teste - anotações do que falta

--- teste.py
+++ teste.py
@@ -16,13 +16,15 @@ with app.app_context():
 
 
 
-# Criar uma função decodificação jwt - validação nas rotas - JWT
+# Criar testes de integração com o banco
+# Swagger
+# Relatório
 # 3° Arrumar os testes
-# 2° JTW
-# 1° Docker (configuração)
 # 4° Deploy 
 # --------------------
 # - Passar para o MYSQL (Baixar e configurar)
+# Criar uma função decodificação jwt - validação nas rotas - JWT
+# 2° JTW
 # - Mexer com o Docker
 # - Deploy
 #Tem que criar um método para calcular idade (aluno) dentro da classe
@@ -32,3 +34,29 @@ with app.app_context():
 '''--------------------------------------------------------------'''
 # docker build -t saladeaula . - cria imagem (imagem é uma receita para o container)
 # docker run -p 8001:8001 saladeaula professor - cria container
+
+# duplicacao = verificar_duplicacao(dados['id'])
+    # if duplicacao:
+    #     return duplicacao
+
+    # def verificar_duplicacao(id):
+#     if Turma.query.get(id):
+#         return jsonify({"error": f"Turma com id {id} já existe."}), 400
+#     return None
+
+
+# def setUp(self):
+#     self.app = app.test_client()
+#     self.app_context = app.app_context()
+#     self.app_context.push()
+    
+#     # Limpa as tabelas manualmente
+#     from models.modelAluno import Aluno
+#     from models.modelProfessor import Professor
+#     from models.modelTurma import Turma
+
+#     db.session.query(Aluno).delete()
+#     db.session.query(Turma).delete()
+#     db.session.query(Professor).delete()
+#     db.session.commit()
+
