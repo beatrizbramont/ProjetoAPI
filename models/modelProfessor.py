1a2e1ea4232372dad2dfd84b3e6131f9eba98e06
Beatriz <beatriz.bramont@gmail.com>
2025-04-25 18:05:51 -0300

    fix: modelProfessor - atualização do código para o MYSQL

--- models/modelProfessor.py
+++ models/modelProfessor.py
@@ -2,6 +2,7 @@ from flask import jsonify
 from config import db
 
 class Professor(db.Model):
+    __tablename__= 'Professor'
     id = db.Column(db.Integer, primary_key=True)
     nome = db.Column(db.String(100))
     idade = db.Column(db.Integer)
