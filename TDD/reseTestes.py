9332342e1d331e93698004d6319a82fff6e87ae7
Beatriz <beatriz.bramont@gmail.com>
2025-04-29 14:49:41 -0300

    fix: resetestes - Ajustados

--- TDD/reseTestes.py
+++ TDD/reseTestes.py
@@ -5,11 +5,9 @@ def resetar_banco():
     try:
         with app.app_context():
             with db.engine.connect() as connection:
-                connection.execute(text("PRAGMA foreign_keys = OFF;"))
                 connection.execute(text("DELETE FROM Aluno;"))
                 connection.execute(text("DELETE FROM Turma;"))
                 connection.execute(text("DELETE FROM Professor;"))
-                connection.execute(text("PRAGMA foreign_keys = ON;"))
                 connection.commit()
             print("Banco resetado com sucesso.")
     except Exception as e:
